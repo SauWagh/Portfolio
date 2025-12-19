df93f1826df276e62456d0d161270d15f713fe43
SauWagh <waghmaresaurabh332@gmail.com>
2025-12-19 15:11:20 +0530

    Updated Code

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -25,13 +25,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv("SECRET_KEY", "fallback_dev_key") 
+SECRET_KEY = os.getenv("SECRET_KEY")  # no fallback in production
+DEBUG = os.getenv("DEBUG", "False") == "True"
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = True
 
-DEBUG = 'RENDER' not in os.environ  
+# DEBUG = 'RENDER' not in os.environ  
 
 if DEBUG:
     MEDIA_URL = '/media/'
