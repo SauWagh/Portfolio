28f7bb3f6b004ca6e76578c0cdfe59c4238b1cec
SauWagh <waghmaresaurabh332@gmail.com>
2025-12-29 14:47:33 +0530

    updated code

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -43,6 +43,9 @@ ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = [
+    'cloudinary',
+    'cloudinary_storage',
+    
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -50,8 +53,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'portfolio_app',
-    'cloudinary',
-    'cloudinary_storage',
 ]
 
 MIDDLEWARE = [
